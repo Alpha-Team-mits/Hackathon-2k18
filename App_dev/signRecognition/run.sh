b1c59c9a0cea4bf831416cf9e6b97cff6e7ceea2
Alpha-Team-mits <eldhoseka.kuzhimalayil@gmail.com>
2018-03-18 11:51:05 +0530

    final

--- App_dev/signRecognition/run.sh
+++ App_dev/signRecognition/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+a=100
+echo $a
 while [ 1 ]
 do
 python detectWithWebCamera.py | awk '{ if( $1=="Turn" ){print "Decelerating Vehicle"; }}'
+a=`expr $a - 10`
+echo $a
+if [ $a -lt 41 ]
+then
+    exit
+fi
 done
